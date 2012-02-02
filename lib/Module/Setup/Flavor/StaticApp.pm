3dc1f12047f971f04d677607c6acf0123206e619
okamuuu <okamuuu@gmail.com>
2012-02-02 11:12:00 +0900

    initial commit

---
+++ lib/Module/Setup/Flavor/StaticApp.pm
@@ -0,0 +1,32 @@
+package Module::Setup::Flavor::StaticApp;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Module::Setup::Flavor::StaticApp -
+
+=head1 SYNOPSIS
+
+  use Module::Setup::Flavor::StaticApp;
+
+=head1 DESCRIPTION
+
+Module::Setup::Flavor::StaticApp is
+
+=head1 AUTHOR
+
+okamuuu E<lt>okamuuu {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
